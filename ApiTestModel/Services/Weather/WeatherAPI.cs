a46894f7758d6f08043efb9ed7195efc51b610a8
serafimtcnu <serafim97@yahoo.com>
2023-04-03 19:35:07 +0300

    init

---
+++ ApiTestModel/Services/Weather/WeatherAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Services.Weather
+{
+    public class WeatherAPI
+    {
+        private HttpClient _restClient = new HttpClient();
+        private string URI = "https://api.weather.gov/alerts?area=MN&severity=severe";
+        public async Task<string> Get_ReasonPhrase()
+        {
+            //BUILD URI
+            _restClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml,application/xml");
+            _restClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+            _restClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+            _restClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Charset", "ISO-8859-1");
+        
+            //GET CALL
+            var response = await _restClient.GetAsync(URI);
+
+            //Read the ReasonPhrase
+            var reasonPhrase = response.ReasonPhrase.ToString();
+            return reasonPhrase;
+        }
+    }
+}
+ 
---
+++ ApiTestModel/TestProject1/WeatherAPI/WeatherAPI_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.Weather;
+namespace TestProject.WeatherAPI
+{
+    public class WeatherAPI_Tests
+    {
+        [Test]
+        [Category("APITests")]
+        public async Task API_Test()
+        {
+            Services.Weather.WeatherAPI weatherAPI = new Services.Weather.WeatherAPI();
+            var reasonPhrase = await weatherAPI.Get_ReasonPhrase();
+            Assert.AreEqual("OK", reasonPhrase);
+        }
+    }
+}
